2c6ffaec33f55ba01bf7a55ff788c1d13d6eb5e3
HexaKhan <hexakhan@protonmail.com>
2018-07-31 22:17:25 -0400

    First production test!

--- badgeCode.c
+++ badgeCode.c
@@ -2,6 +2,9 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+//Global Variables
+int funMode = 0;
+
 void setupPins(){
   DDRB = ~(1 << PB5);       // Set all pins as output except PB5 (button)
   PORTB = (1 << PB5);    		// Enable pullup resistor
@@ -47,19 +50,26 @@ void runRoulette(uint8_t startPin){
 		}
 		PORTB = currentLED | (1 << PB5);		// Light the LED
 
-    if (delayTime >= 300){
+    if (delayTime >= 250){
       delayIncrement = 30;
+    }
+
+    if (delayTime >= 400){
+      delayIncrement = 50;
     }
 
 		if (buttonReleased){									// Button has already been released, check for winner or increment delay time
 
 			if (delayTime > maxDelay){										// Winner Winner Chicken Dinner! Flash the winner
-				delay_ms(maxDelay);
+				delay_ms(delayTime*2);
 				for (int i = 0; i < flashCount*2; i++){
 					PORTB ^= currentLED;	//	Toggle LED
 					delay_ms(flashSpeed);
 				}
         PORTB = (1 << PB5);     // Ensure LEDs are turned off
+        if (funMode){
+          delay_ms(750);       // Delay helps with special effects (or else they start too soon)
+        }
 				break;									//	Break out of runRoulette
 			}
       else{
@@ -74,9 +84,11 @@ void runRoulette(uint8_t startPin){
 	}
 }
 
+//Variables for interrupt
 volatile int timesPressed = 0;
 volatile int programRunning = 0;
 volatile uint8_t startingPin = (1 << PB0);
+
 ISR(TIMER0_OVF_vect){
   if (buttonPressed() && !programRunning){  // Checking for program running may not be needed at all..
     timesPressed++;
@@ -132,27 +144,67 @@ void flashLightsPattern(int delay_time){
   delay_ms(delay_time);
 }
 
+void fanPattern(int delay_time){
+  uint8_t pattern[4] = {0b00000000,0b00000100,0b00001110,0b00011111};
+  for (int i = 0; i < 4; i++){
+    PORTB = pattern[i] | (1 << PB5);
+    delay_ms(delay_time);
+  }
+  for (int i = 3 - 1; i >= 1; i--){
+    PORTB = pattern[i] | (1 << PB5);
+    delay_ms(delay_time);
+  }
+  PORTB = (1 << PB5);
+}
+
+void triangleSpinPattern(int delay_time){
+  uint8_t pattern[5] = {0b00010110,0b00001011,0b0010101,0b00011010,0b00001101};
+  for (int i = 0; i < 4; i++){
+    PORTB = pattern[i] | (1 << PB5);
+    delay_ms(delay_time);
+  }
+}
+
 void runFunMode(){
   spinClearPattern(500);
   randomAppearPattern(1000);
   randomClearPattern(1000);
+  triangleSpinPattern(500);
   flashLightsPattern(500);
   flashLightsPattern(500);
-  flashLightsPattern(500);
+  fanPattern(500);
+  fanPattern(500);
+  spinClearPattern(250);
+  randomAppearPattern(500);
+  randomClearPattern(500);
+  triangleSpinPattern(250);
+  flashLightsPattern(250);
+  flashLightsPattern(250);
+  fanPattern(250);
+  fanPattern(250);
+  spinClearPattern(125);
+  randomAppearPattern(250);
+  randomClearPattern(250);
+  triangleSpinPattern(125);
+  flashLightsPattern(125);
+  flashLightsPattern(125);
+  fanPattern(125);
+  fanPattern(125);
 }
 
 int main(void) {
   setupPins();
   setupTimer();
-  int funMode = 0;
   if (buttonPressed()){   // If button is held on start, do special effects while idle
     funMode = 1;
   }
-  while (1){
-    if (funMode){
+  if (funMode){
+    while (1){
       runFunMode();
     }
-    else {
+  }
+  else {
+    while (1){
       continue;
     }
   }
